2d2d54171d80f253b61f69a8ad9f9b22246be393
zliendo <zenobiosky@yahoo.com>
2018-04-16 22:05:49 -0400

    js api

---
+++ idannet/js/client_idannet.js
@@ -0,0 +1,25 @@
+// Author: Zenobia Liendo
+
+
+// function called from the client
+function link_idannet_frame() {
+    var  myIframe = document.getElementById('idannet_frame');
+    myIframe.contentWindow.addEventListener('message', function(event) {
+	info_json = JSON.parse(event.data)
+	 myIframe.contentWindow.patient_info = info_json[0];
+	 myIframe.contentWindow.idannet_response =info_json[1];
+
+         }, false);       
+
+}
+
+window._sendMessage = function(patient_info, idannet_response) {
+    // read data from json input 
+    var  info = [patient_info, idannet_response];
+    var  myIframe = document.getElementById('idannet_frame');
+    myIframe.contentWindow.postMessage(JSON.stringify( info), '*');
+}
+
+
+
+
